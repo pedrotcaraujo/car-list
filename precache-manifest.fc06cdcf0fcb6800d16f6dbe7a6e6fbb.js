a6ca9cc1bb58e9a2d8bb84ffe636254a31c108b9
Pedro Araujo <pedrotcaraujo@gmail.com>
2019-12-05 05:48:45 -0800

    Updates

---
+++ precache-manifest.fc06cdcf0fcb6800d16f6dbe7a6e6fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99be91983ffbd70d9a33cb3e7a116f33",
+    "url": "/car-list/index.html"
+  },
+  {
+    "revision": "f300c18ec3bcbc0d84de",
+    "url": "/car-list/static/css/main.b5a2c14d.chunk.css"
+  },
+  {
+    "revision": "72f42390f34c5e537afe",
+    "url": "/car-list/static/js/2.d4ff286e.chunk.js"
+  },
+  {
+    "revision": "f300c18ec3bcbc0d84de",
+    "url": "/car-list/static/js/main.bac134b7.chunk.js"
+  },
+  {
+    "revision": "5ae3cf3fc428be5ab815",
+    "url": "/car-list/static/js/runtime-main.e06c2292.js"
+  }
+]);
